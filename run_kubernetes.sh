07e28ce39235b8a60afde0fab006669448d63533
EC2 Default User <ec2-user@ip-172-31-18-70.us-west-2.compute.internal>
2020-05-27 12:07:49 +0000

    Updating run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=emjotpe/clouddevops_project5
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run project4 --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/project4 8080:80
 
